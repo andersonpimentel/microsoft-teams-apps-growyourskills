7922e6544634e419e5a2f4f68bbcd1133a9bc605
Anderson dos Santos Pimentel <anderson.pimentel@petrobras.com.br>
2021-03-11 13:47:46 -0300

    Substituindo o termo "projeto" por "desafio", a pedido da Comunicação (via Rigotti)

--- Source/Microsoft.Teams.Apps.Grow/Resources/Strings.Designer.cs
+++ Source/Microsoft.Teams.Apps.Grow/Resources/Strings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.Teams.Apps.Grow {
+namespace Microsoft.Teams.Apps.Grow.Resources {
     using System;
     
     
