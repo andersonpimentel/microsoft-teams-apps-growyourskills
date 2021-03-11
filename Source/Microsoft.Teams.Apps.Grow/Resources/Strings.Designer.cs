fb74e8a403652bb180ffc1a43132ee525e0dd8d0
Anderson dos Santos Pimentel <anderson.pimentel@petrobras.com.br>
2021-03-11 14:10:47 -0300

    Corrigindo namespace errado

--- Source/Microsoft.Teams.Apps.Grow/Resources/Strings.Designer.cs
+++ Source/Microsoft.Teams.Apps.Grow/Resources/Strings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.Teams.Apps.Grow.Resources {
+namespace Microsoft.Teams.Apps.Grow {
     using System;
     
     
